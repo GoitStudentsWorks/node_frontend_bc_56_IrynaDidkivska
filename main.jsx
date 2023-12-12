d267d5c7176f1634f5f5aa337a85c995fd574532
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-12-12 16:44:23 +0200

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -14,7 +14,7 @@ import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate loading={null} persistor={persistor}>
     <Provider store={store}>
-      <BrowserRouter basename="/node_frontend_bc_56_IrynaDidkivska">
+      <BrowserRouter basename="/node_frontend_bc_56_IrynaDidkivska/">
         <App />
         <ToastContainer autoClose={1800} />
       </BrowserRouter>
